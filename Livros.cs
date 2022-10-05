4ac46e241d5c95dc97e164a5f8039f023f7e4045
Leme-do-Prado <102317613+Leme-do-Prado@users.noreply.github.com>
2022-10-05 16:24:16 -0300

    Create Livros.cs

---
+++ Livros.cs
@@ -0,0 +1,11 @@
+public class Livros{
+    List<livro> acervo;
+
+    public void adicionar(Livro livro){
+        acervo.add(livro);
+    }
+
+    public Livro pesquisar(Livro livro){
+        
+    }
+}
