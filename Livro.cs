4b528d7ea02423b2f99624777ee40aed600d8ca8
Leme-do-Prado <102317613+Leme-do-Prado@users.noreply.github.com>
2022-10-05 16:14:52 -0300

    Create Livro.cs

---
+++ Livro.cs
@@ -0,0 +1,61 @@
+public class Livro{
+    int isbn;
+    string titulo;
+    string autor;
+    string editora;
+    List<Exemplar> exemplares;
+
+    public void adicionarExemplar(Exemplar exemplar){
+        exemplares.Add(exemplar);
+    }
+
+    public int qtdeExemplares(){
+        int i = 0;
+        foreach(Exemplar in exemplares){
+            i++;
+        }
+return i;
+    }
+
+    public int qtdeDisponiveis(){
+        int i = 0;
+        foreach(Exemplar in exemplares){
+            if(disponivel == true){
+                i++
+            }
+        }
+        return i;
+    }
+
+    public int qtdeEmprestimos(){
+        int i = 0;
+        foreach(Exemplar in exemplares){
+            if(emprestar == true){
+                i++
+            }
+        }
+        return i;
+    }
+
+    public double percDisponibilidade(){
+        int i = qtdeDisponiveis;
+        int j = qtdeExemplares;
+        double perc;
+
+        perc = i/j * 100;
+
+        return perc;
+    }
+
+}
+
+public class Exemplar{
+    int tombo;
+    List<Emprestimo> emprestimos;
+
+    public bool emprestar(){
+        emprestado == true;
+        dispo
+    }
+}
+
